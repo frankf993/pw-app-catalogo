f48c04bfb36e53ce1f4b7a5cb97d4a372b5c138f
francesco fumarola <francesco.fumarola12@hotmail.com>
2023-11-09 20:50:55 +0100

    cardProdotto to mod

--- src/components/prodotto/prodotto-dettaglio.tsx
+++ src/components/prodotto/prodotto-dettaglio.tsx
@@ -1,3 +1,4 @@
+import Card from 'react-bootstrap/Card';
 interface IProps {
 
 }
@@ -6,9 +7,17 @@ const ProdottoDettaglio: React.FC<IProps> = (props: IProps): React.ReactElement 
 
 
     return (
-        <>
-            Card con i dettagli del prodotto
-        </>
+        <Card>
+        <Card.Img variant="top" src="holder.js/100px160" />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            This is a longer card with supporting text below as a natural
+            lead-in to additional content. This content is a little bit
+            longer.
+          </Card.Text>
+        </Card.Body>
+      </Card>
     );
 };
 
